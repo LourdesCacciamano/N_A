d07a669542067b2625f01ac788a283875af1231e
LourdesCacciamano <lourdescacciamano@gmail.com>
2026-02-06 20:00:08 -0300

    agregue estilo al timer y responsive

--- javaScript/timer.js
+++ javaScript/timer.js
@@ -2,8 +2,8 @@ const estado = document.getElementById("estado");
 const tiempo = document.getElementById("tiempo");
 const tick = document.getElementById("tick");
 const beep = document.getElementById("beep");
-const longBeep = document.getElementById("longBeep");
 const pausaBtn = document.getElementById("pausar");
+const restablecer = document.getElementById("restablecer");
 
 let timer = null;
 let segundos = 0;
@@ -14,24 +14,26 @@ let totalRondas = 0;
 let totalCiclos = 0;
 let tiempos = {};
 let descansoActual = null;
-
+let tipoEstado = "";
+let enCuentaFinal = false;
 //let estadoActual = "";
 
+function playBeep () {
+    if (!beep.paused) return; // evita que se reinicie el audio
+    beep.currentTime = 0;
+    beep.play();
+}
+
+function playTick() {
+    if (!tick.paused) return;
+    tick.currentTime = 0;
+    tick.play();
+}
 
 function formatoTiempo(s) {
     const m = Math.floor(s/60);
     const sec = s % 60;
     return `${m.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
-}
-
-function sonarUltimosSeg(seg) {
-    if (seg <= 3 && seg > 0) {
-        beep.currentTime = 0;
-        beep.play();
-    } else{
-        tick.currentTime = 0;
-        tick.play();
-    }
 }
 
 function iniciarCuenta(nombre, duracion, restante) {
@@ -40,31 +42,63 @@ function iniciarCuenta(nombre, duracion, restante) {
     //estadoActual = nombre;
     descansoActual = restante;
     pausado = false;
+    enCuentaFinal = false;
     estado.textContent = nombre;
+    animarEstados();
     segundos =duracion;
     tiempo.textContent = formatoTiempo(segundos);
-    longBeep.currentTime = 0;
-    longBeep.play();
+
+    tipoEstado = (nombre == "PREPÁRATE") ? "PREP" : "NORMAL";
+
+    if (tipoEstado === "PREP" && segundos > 0) {
+            playBeep();
+    } else if (tipoEstado === "NORMAL") {
+            playTick();
+    }
 
     timer = setInterval(cuentaRegresiva, 1000);
 }
 
 function cuentaRegresiva() {
-    if (pausado) return;
+    if (pausado) {
+        tick.pause(); //pausa el sonido si esta sonando
+        return;
+    }
+
+    tiempo.textContent = formatoTiempo(segundos);
+
+    if (tipoEstado === "PREP" && segundos > 0) {
+        if (beep.paused) {
+            playBeep();
+        }
+    }
+
+    if (segundos <= 3 && segundos > 0 && !enCuentaFinal) {
+        enCuentaFinal = true;
+        playBeep();
+        tick.pause();
+    } 
+    
+    if (segundos > 3 && tipoEstado === "NORMAL") {
+        if (tick.paused) {
+            tick.loop = true; // hace que el sonido suene continuo
+            tick.play();
+        }
+    }
 
     segundos--;
-    tiempo.textContent = formatoTiempo(segundos);
-    sonarUltimosSeg(segundos);
 
-    if(segundos <= 0){
+
+    if (segundos < 0) {
         clearInterval(timer);
+        tick.pause() ; //detiene el sonido al finalizar
         descansoActual && descansoActual();
     }
 }
 
 function iniciarRonda() {
     iniciarCuenta(
-        `ENTRENAR (Ronda ${rondaActual}/${totalRondas})`,
+        `ENTRENAR: Ronda ${rondaActual}/${totalRondas}`,
         tiempos.entrenar,
         iniciarDescansoRonda
     );
@@ -82,7 +116,7 @@ function iniciarDescansoRonda() {
                 if (cicloActual < totalCiclos) {
                     iniciarDescansoCiclo();
                 } else{
-                    estado.textContent = "ENTRENAMIENTO COMPLETADO 💪";
+                    estado.innerHTML = `ENTRENAMIENTO COMPLETADO <i class="fa-regular fa-circle-check entrComp" style="color: #000000;"></i`;
                 }
             }
         }
@@ -98,7 +132,7 @@ function iniciarDescansoCiclo() {
     }
 
     iniciarCuenta(
-        `DESCANSO ENTRE CICLOS (${cicloActual}/${totalCiclos})`,
+        `DESCANSO ENTRE CICLOS: ${cicloActual}/${totalCiclos}`,
         tiempos.descansarCiclo,
         () => {
             cicloActual++;
@@ -108,8 +142,22 @@ function iniciarDescansoCiclo() {
     );
 }
 
+function desbloquearAudio() {
+    beep.play().then(() => {
+        beep.pause();
+        beep.currentTime = 0;
+    }). catch(() => {});
+}
+
+function animarEstados () {
+    estado.classList.remove("animate__animated" , "animate__fadeInRight");
+    void estado.offsetWidth;
+    estado.classList.add("animate__animated", "animate__fadeInRight");
+}
+
 
 document.getElementById("empezar").addEventListener("click", () => {
+    beep.play().then(() => {beep.pause(); beep.currentTime = 0;}).catch(() => {});
     pausaBtn.textContent = "⏸️ PAUSAR";
 
     tiempos = {
@@ -133,4 +181,32 @@ pausaBtn.addEventListener("click", () => {
 
     pausado = !pausado;
     pausaBtn.textContent = pausado ? "▶️ REANUDAR" : "⏸️ PAUSAR";
+
+    if (pausado) {
+        tick.pause(); //pausa el sonido si esta sonando
+        beep.pause();
+    } else {
+        if(tipoEstado === "NORMAL" && segundos > 3) tick.play();
+        else if (tipoEstado === "PREP" && segundos > 0) beep.play();
+    }
+});
+
+restablecer.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = null;
+    pausado = false;
+
+    segundos = 0;
+    rondaActual = 1;
+    cicloActual = 1;
+
+    estado.textContent = "PREPÁRATE";
+    tiempo.textContent = "00:00";
+
+    pausaBtn.textContent = "⏸️ PAUSAR";
+
+    [tick, beep].forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
 });
